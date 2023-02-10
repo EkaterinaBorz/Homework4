fe11e928a7c9f3a0d403aab884a022b5f2a3b26a
ekaterina <whitedevil93@mail.ru>
2023-02-11 01:26:40 +0300

    arraMinMax

---
+++ дз4.3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.Clear();
+int[] array = { 2, 32, 41, 15, 46, 70, 28, 91, 14, 18 };
+int max = array[0];
+int min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max) max = array[i];
+    if (array[i] < min) min = array[i];
+}
+Console.WriteLine();
+Console.Write("Разница между максимальным и минимальным элементом = ");
+Console.Write(max - min);
